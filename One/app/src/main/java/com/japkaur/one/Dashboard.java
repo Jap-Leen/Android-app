8199cff70bbe614617a85b8a12e882539fc86882
Jap Leen Kaur Jolly <jap.jolly@gmail.com>
2018-01-27 10:28:34 +0530

    added logout feature

--- One/app/src/main/java/com/japkaur/one/Dashboard.java
+++ One/app/src/main/java/com/japkaur/one/Dashboard.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -16,11 +17,15 @@ import java.util.HashMap;
 public class Dashboard extends Activity {
     // User Session Manager Class
     UserSessionManager session;
+    // Button Logout
+    Button btnLogout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Set View to activity.xml
         setContentView(R.layout.activity_dashboard);
+        // Button logout
+        btnLogout = (Button) findViewById(R.id.btnLogout);
         FloatingActionButton fab;
         // Session class instance
         session = new UserSessionManager(getApplicationContext());
@@ -44,8 +49,19 @@ public class Dashboard extends Activity {
         if(session.checkLogin())
             finish();
 
+
         // get user data from session
         HashMap<String, String> user = session.getUserDetails();
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Clear the User session data
+                // and redirect user to LoginActivity
+                session.logoutUser();
+            }
+        });
     }
 
 
